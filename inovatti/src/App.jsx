bc0f9104952249710d025e5d397cd10d3d7fd1bb
Alexandre Cruv <alefeufei@gmail.com>
2025-04-02 20:53:24 -0300

    inicio do header
    comecei a configurar o header

---
+++ inovatti/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './assets/components/Header'
+
+function App() {
+  
+  return (
+    <>
+    <Header />
+      <h1>Inovatti</h1>
+      <h2>Test</h2>
+      <p>Test</p>
+      <p>Test</p>
+    </>
+  )
+}
+
+export default App
---
+++ inovatti/src/assets/components/Header/index.jsx
@@ -0,0 +1,21 @@
+import "./Header.module.css"; // Importing CSS for styling
+
+function Header() {
+  return (
+    <header>
+      <img
+        src="https://inovattitecnologia.com.br/wp-content/uploads/2020/09/novaLogo2.png"
+        alt="Logo Inovatti"
+        className="logo" width={200} // Added width and height attributes for better performance
+      />
+      <nav>
+        <li>Home</li>
+        <li>Serviços</li>
+        <li>Sobre nós</li>
+        <li>Contatos</li>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
