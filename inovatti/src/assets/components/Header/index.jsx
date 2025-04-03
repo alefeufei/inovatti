441d3cec524e2d8d6f0681f56f23c06a55a98e4a
Alexandre Cruv <alefeufei@gmail.com>
2025-04-03 20:59:17 -0300

    consertando css do header

--- inovatti/src/assets/components/Header/index.jsx
+++ inovatti/src/assets/components/Header/index.jsx
@@ -3,17 +3,22 @@ import styles from "./Header.module.css"; // Importing CSS for styling
 function Header() {
   return (
     <header className={styles.header}>
-      <img
-        src="https://inovattitecnologia.com.br/wp-content/uploads/2020/09/novaLogo2.png"
-        alt="Logo Inovatti"
-        className="logo"
-        width={200} // Added width and height attributes for better performance
-      />
       <nav>
-        <li>Home</li>
-        <li>Serviços</li>
-        <li>Sobre nós</li>
-        <li>Contatos</li>
+        <div className="logo">
+          <img
+            src="https://inovattitecnologia.com.br/wp-content/uploads/2020/09/novaLogo2.png"
+            alt="Logo Inovatti"
+            className="logo"
+            width={150}
+            height={30} // Added width and height attributes for better performance
+          />
+        </div>
+        <ul>
+          <li className={styles["nav-item"]}>Home</li>
+          <li className={styles["nav-item"]}>Serviços</li>
+          <li className={styles["nav-item"]}>Sobre nós</li>
+          <li className={styles["nav-item"]}>Contatos</li>
+        </ul>
       </nav>
     </header>
   );
