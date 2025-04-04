92b4f90009f90d9c288e16ce9ace19a45e93b80c
Alexandre Cruv <alefeufei@gmail.com>
2025-04-03 23:30:51 -0300

    header incompleto
    header incompleto

--- inovatti/src/assets/components/Header/index.jsx
+++ inovatti/src/assets/components/Header/index.jsx
@@ -3,22 +3,24 @@ import styles from "./Header.module.css"; // Importing CSS for styling
 function Header() {
   return (
     <header className={styles.header}>
-      <nav>
-        <div className="logo">
+      <nav className={styles.navbar}>
+        <div>
           <img
             src="https://inovattitecnologia.com.br/wp-content/uploads/2020/09/novaLogo2.png"
             alt="Logo Inovatti"
-            className="logo"
-            width={150}
-            height={30} // Added width and height attributes for better performance
-          />
+            className={styles["logo"]}/>
         </div>
         <ul>
           <li className={styles["nav-item"]}>Home</li>
           <li className={styles["nav-item"]}>Serviços</li>
           <li className={styles["nav-item"]}>Sobre nós</li>
-          <li className={styles["nav-item"]}>Contatos</li>
+          <li className={styles["nav-item"]}>Abrir um Chamado</li>
+          <li className={styles["nav-item"]}>Blog</li>
+          <li className={styles["nav-item"]}>Contato</li>
         </ul>
+        <div>
+          <button className={styles["agendar-visita"]}>AGENDAR VISITA</button>
+        </div>
       </nav>
     </header>
   );
